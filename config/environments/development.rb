f13ee1e42fa519e357cc8214f6c1a98c6e41c0b3
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-22 20:28:17 +0000

    Add host configuration for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "effective-space-waddle-vwrv97gxw5q3xq54-3000.app.github.dev"
 end
